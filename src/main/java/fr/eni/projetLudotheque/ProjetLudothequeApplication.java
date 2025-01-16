a206c02197b4946573899ccae4a5dd8d1c321100
amir <amirben1991@gmail.com>
2025-01-16 11:27:55 +0100

    first push, new project

---
+++ src/main/java/fr/eni/projetLudotheque/ProjetLudothequeApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.projetLudotheque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetLudothequeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetLudothequeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/eni/projetLudotheque/ProjetLudothequeApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.projetLudotheque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetLudothequeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
