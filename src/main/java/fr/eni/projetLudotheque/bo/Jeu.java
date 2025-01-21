dfea387a832b85883e4964944700420a45d70f39
amir <amirben1991@gmail.com>
2025-01-21 22:12:17 +0100

    add backlog file

--- src/main/java/fr/eni/projetLudotheque/bo/Jeu.java
+++ src/main/java/fr/eni/projetLudotheque/bo/Jeu.java
@@ -1,7 +1,14 @@
 package fr.eni.projetLudotheque.bo;
 
+import java.util.List;
+
+// TODO: résoudre le souci au niveau du gradle et des importations des librairies
+//import jakarta.validation.constraints.NotBlank;
+//import jakarta.validation.constraints.Min;
+
 public class Jeu {
 	
+//	@NotBlank
 	private Integer noJeu;
 	
 	private String titre;
@@ -12,5 +19,13 @@ public class Jeu {
 	
 	
 	
+//	
+//	private Float tarifJournée;
+//	
+//	private int ageMin;
+//	
+//	private int duree;
+//	
+//	private List<Genre> genres;
 
 }
